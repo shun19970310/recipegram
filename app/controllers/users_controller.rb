f5cd34b00ebe8fd92787091ec5c696df97d68cbd
EC2 Default User <ec2-user@ip-172-31-84-67.ec2.internal>
2022-03-30 04:47:18 +0000

    [Add]ユーザー一覧とユーザー詳細ページ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,9 @@ class UsersController < ApplicationController
   end
 
   def show
+    # 一人のユーザーの情報を取得し、@userに代入している
+    # params[:id]はカーソルを合わせた時に出てくるusers/1といった各々のidを持ってきている
+    @user = User.find(params[:id])
   end
 
   def edit
